b0ab07b03d00c610f059e822ce4ec5122a8ee562
aonikpaulalock <alockpaul9845@gmail.com>
2022-06-10 20:58:31 +0600

    upadated navber

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
     }} id="#baneer">
       <div class="hero-content text-center">
         <div class="max-w-lg text-gray-300">
-          <h3 class="md:text-2xl text-xl font-medium tracking-wider mb-5">HELLO I Am </h3>
+          <h3 class="md:text-2xl text-xl font-medium tracking-wider mb-5">Hello I Am </h3>
           <h1 class="md:text-5xl text-4xl font-bold">Aonik Paul Alock</h1>
           <h3 class="md:text-2xl text-xl font-medium racking-wider pt-3 pb-7 text-neutral">Junior Frontend Developer</h3>
           <a href="https://drive.google.com/file/d/1b0zAMeCqfmaAesCnrQJJ3PCao-E67nyZ/view?usp=sharing" download target="_blank" as={Link} rel="noreferrer">
--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -14,26 +14,34 @@ const Header = () => {
     </>
 
   return (
-    <div class="navbar md:px-14 sm:px-6 shadow-lg py-4 sticky top-0 bg-white z-30">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow-md rounded-box w-52 text-md font-medium text-info">
+    <div className="sticky top-0 bg-white z-30">
+      <div class="navbar lg:px-16 sm:px-6 shadow-lg py-4">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabindex="0" class="menu menu-compact dropdown-content mt-5 p-2 shadow-md rounded-box w-60 text-md font-medium bg-white">
+              {navMenu}
+            </ul>
+          </div>
+          <Link smooth to="#" class="text-secondary font-bold text-3xl tracking-wider xs:ml-14 ml-3">
+            Ap<span className='text-neutral'>Aonik</span>
+          </Link>
+        </div>
+        <div class="navbar-end hidden lg:flex z-0 text-gray-500">
+          <ul class="menu menu-horizontal p-0 text-md font-medium ">
             {navMenu}
           </ul>
         </div>
-        <Link smooth to="#" class="text-secondary font-bold text-3xl tracking-wider">
-          Ap<span className='text-neutral'>Aonik</span>
-        </Link>
-      </div>
-      <div class="navbar-end hidden lg:flex">
-        <ul class="menu menu-horizontal p-0 text-md font-medium text-secondary">
-          {navMenu}
-        </ul>
       </div>
     </div>
+
+
+
+
+
+
   );
 };
 
